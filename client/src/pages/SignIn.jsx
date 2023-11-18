c36dd5778c1c1b7585d685957091dc06efa8fb33
zisan <mashroorzisan@gmail.com>
2023-11-18 14:21:52 +0600

    added the redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,13 +1,15 @@
 import { set } from "mongoose";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector} from "react-redux";
+import { signInStart,signInFailure,signInSuccess } from "../redux/user/userSlice";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [created, setCreated] = useState(null);
+
+  const {loading, error} = useSelector((state)=>state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     // console.log(e.target);
@@ -19,7 +21,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch("api/auth/signin", {
         method: "POST",
         headers: {
@@ -30,19 +32,13 @@ export default function SignIn() {
       const data = await res.json();
       console.log(data);
       if (data.success === false) {
-        setError(data.message);
-        setCreated(null); //usercreation unsuccessful
-        setLoading(false);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setError(null);
-      setCreated(data); //usercreation successful
-      setLoading(false);
+      dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
-      setCreated(null); //usercreation unsuccessful
-      setError(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
   };
   return (
@@ -80,8 +76,12 @@ export default function SignIn() {
         </Link>
       </div>
       {/* after creation */}
-      {created && <p className="text-green-500 mt-5">{created}</p>}
-      {error && <p className="text-red-500 mt-5">{error}</p>}
+       {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
+
+
+
+//username: test@pmail.com
+//password: test@pmail.com
